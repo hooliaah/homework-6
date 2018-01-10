6531d324574df79682c608ee11ea88751fdddfd4
Julie Groth <groth.julie@gmail.com>
2018-01-09 20:31:22 -0800

    updated functionality to display difs when button clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,29 +13,41 @@ function displayButtons() {
 }
 displayButtons();
 
-
+// user can add a new button using search box 
+$("#add-animal").on("click", function (event) {
+    event.preventDefault();
+    var newAnimal = $("#animal-input").val().trim();
+    console.log(newAnimal);
+    animals.push(newAnimal);
+    displayButtons();
+})
 
 // when button is clicked, 10 gifs of that animal apper
-var animalName;
+$("#animal-div").on("click", ".animal-button", function(animal){
+    event.preventDefault();
 
-// $("#find-animal").on("click", function(animal){
-//     event.preventDefault();
+    var animalName = $(this).data("name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=5LCGKiln2CYkrYWGgqJS7gbm25PsNcg2&limit=10&rating=pg";
 
-//     var animal = $("#animal-input").val();
-//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=5LCGKiln2CYkrYWGgqJS7gbm25PsNcg2";
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    console.log(response);
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).done(function(response) {
-//     console.log(response);
-//   });
-// });
+    var results = response.data;
 
-// $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-//         $(".wind").text("Wind Speed: " + response.wind.speed);
-//         $(".humidity").text("Humidity: " + response.main.humidity);
-//         $(".temp").text("Temperature (F) " + response.main.temp);
+    for (var j = 0; j < results.length; j++) {
 
-// user can add a new button using search box 
+        var gifDiv = $("<div>");
+        var showRating = $("<p>").text("Rating: " + results[j].rating);
+        var imageTag = $("<img>");
+        imageTag.attr("src", results[j].images.fixed_height_still.url);
 
+        gifDiv.append(showRating, imageTag);
+        $("#animal-gif").append(gifDiv);
+    }
+
+
+  });
+});
